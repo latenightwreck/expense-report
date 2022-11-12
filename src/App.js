7f1427c32169c26aac2dbd16a4bda4632d4ed207
Morgan <morgan.b.armstrong@gmail.com>
2022-11-12 08:10:45 -0500

    begin useState. Not sure if this is the best way

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/NewExpense/NewExpense';
 
 function App() {
   const expenses = [
@@ -23,7 +24,12 @@ function App() {
     },
   ];
 
-  return <Expenses expenses={expenses} />;
+  return (
+    <div>
+      <NewExpense />
+      <Expenses expenses={expenses} />;
+    </div>
+  );
 }
 
 export default App;
