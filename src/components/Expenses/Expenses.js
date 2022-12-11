1240f163d58b87869c2be7c236c3a11420cd5b93
Morgan <morgan.b.armstrong@gmail.com>
2022-12-11 13:16:04 -0500

    Pulled List as its own component Updated semantics

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,14 +3,17 @@ import Card from '../UI/Card';
 import ExpenseItem from './ExpenseItem';
 import './Expenses.css';
 import ExpensesFilter from './ExpensesFilter';
+import ExpensesList from './ExpensesList';
 
 function Expenses(props) {
   const [filterYear, setFilterYear] = useState('2021');
-
-  const expenses = props.expenses;
   const expenseYearFilterHandler = (filteredYear) => {
     setFilterYear(filteredYear);
   };
+
+  const filteredExpenses = props.expenses.filter((expense) => {
+    return expense.date.getFullYear().toString() === filterYear;
+  });
 
   return (
     <Card className='expenses'>
@@ -18,26 +21,7 @@ function Expenses(props) {
         selected={filterYear}
         onYearFilterData={expenseYearFilterHandler}
       />
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      />
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      />
+      <ExpensesList items={filteredExpenses} />
     </Card>
   );
 }
